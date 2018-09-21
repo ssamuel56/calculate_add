2cda77febd86f0af0cf3e1cc1cdbe628a612ce34
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-09-20 22:35:00 -0400

    Had my .to_i in the wrong place

--- calculate_add.rb
+++ calculate_add.rb
@@ -4,9 +4,9 @@ numb = $stdin.gets.chomp.split(" ")
 
 if numb[1] != "+"
   puts "You need to use + i.e. 2 + 2"
-  load 'calculate_add.rb'
+  quit
 else
-  puts numb[0.to_i] + numb[2.to_i]
+  puts numb[0].to_i + numb[2].to_i
 end
 
 puts "\n"
