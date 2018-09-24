24b2af97ea7ae7053d790a60750ed1aa42504eeb
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-09-24 13:54:49 -0400

    added second test file

--- add_tdd.rb
+++ add_tdd.rb
@@ -2,7 +2,10 @@ require_relative 'add.rb'
 require 'minitest/autorun'
 
 class Add_test < Minitest::Test
-  def test_1
-    assert_equal(11, addition(5, 6))
+  def test_1_f
+    assert_equal(1, addition(5, 6))
+  end
+  def test_2_f
+    assert_equal(5, addition(7, 5))
   end
 end
